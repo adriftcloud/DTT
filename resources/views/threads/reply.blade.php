0ad50896d948a93c999f14f967bc720a992bb5e2
niz <niz.tseng@gmail.com>
2018-08-08 23:24:00 +0800

    建立003

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,4 +1,4 @@
-<div class="card card bg-light">
+<div class="card card bg-light m-b-20">
     <div class="card-header">
         <a href="#">{{$reply->owner->name}}</a>
         said {{$reply->created_at->diffForHumans()}}
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,13 +2,9 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row m-b-30">
             <div class="col-md-8 offset-md-2">
-
-
-                <div class="card card bg-light">
-
-
+                <div class="card card bg-light ">
                     <div class="card-header">
                         <a href="#">{{$thread->creator->name}}</a> posted:
                         {{$thread->title}}
@@ -22,11 +18,29 @@
         </div>
 
 
-        <div class="row">
+        <div class="row m-b-30">
             <div class="col-md-8 offset-md-2">
                 @foreach($thread->replies as $reply)
                     @include('threads.reply')
                 @endforeach
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                @if(auth()->check())
+                    <form method="post" action="/threads/{{$thread->id}}/replies">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <textarea id="body" name="body" class="form-control" placeholder="請輸入留言..." rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">送出</button>
+                    </form>
+                @else
+                    <p class="text-center">請先<a href="{{route('login')}}">登入</a>後才可留言</p>
+                @endif
             </div>
         </div>
     </div>
